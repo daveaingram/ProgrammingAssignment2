083ab4dd02cb83d8ae5082f1d6017f930c981e6f
Dave Ingram <dave.ingram@acquia.com>
2014-06-21 10:28:57 -0400

    More comments are a good thing

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,19 @@
 ## Project 2 for R Programming. Caching a solved matrix.
 
 ## Set up a cached matrix where the inverse can also be cached.
+## Example: cm <- makeCacheMatrix(matrix(1:4, ncol=2, nrow=2)) 
 makeCacheMatrix <- function(x = matrix()) {
-        solved <- NULL
+        solved <- NULL # Initialize the solved variable
         set <- function(y) {
-                x <<- y
-                solved <<- NULL
+                x <<- y # resets x to be a new value
+                solved <<- NULL # resets solved to null
         }
-        get <- function() x
-        setSolved <- function(solved) solved <<- solved
-        getSolved <- function() solved
+        get <- function() x # return the original matrix
+        setSolved <- function(solved) solved <<- solved # set the solved matrix
+        getSolved <- function() solved # get the solved matrix
         list(set = set, get = get,
              setSolved = setSolved,
-             getSolved = getSolved)
+             getSolved = getSolved) # returns all of the enclosed functions
 }
 
 ## Solve the matrix. Must be passed an instance of makeCacheMatrix
@@ -22,10 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         solved <- x$getSolved()
+        ## If solved is already set, return the cached version
         if(!is.null(solved)) {
                 message("getting cached matrix")
                 return(solved)
         }
+        ## Otherwise, we solve the matrix, setting the cache for next time
         m <- x$get()
         solved <- solve(m, ...)
         x$setSolved(solved)
